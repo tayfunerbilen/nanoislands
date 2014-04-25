a21a51a86c9bd8097cf47554303ec5ae66dd0102
Vadim Patsev <basvasilich@yandex.ru>
2014-04-25 15:43:14 +0400

    header refactoring

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,6 @@
 nb.define('header', {
     events: {
-        'click .nb-header__button': 'togglePress'
+        'click .nb-header-button': 'togglePress'
     },
 
     /**
@@ -9,7 +9,7 @@ nb.define('header', {
 
     togglePress: function(e) {
         var $target = $(e.target);
-        $target.closest('.nb-header__button').toggleClass('nb-header__button_pressed');
+        $target.closest('.nb-header-button').toggleClass('is-pressed');
 
         if ($target.hasClass('nb-icon_services')) {
             nb.trigger('services-click');
